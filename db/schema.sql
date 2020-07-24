ce1a1114d7f02aaa21561b966137e17b02d072d6
cherjios <slopez0286@gmail.com>
2020-07-23 19:36:18 -0700

    Creating burger db

--- db/schema.sql
+++ db/schema.sql
@@ -10,20 +10,3 @@ CREATE TABLE burgers(
     devoured BOOLEAN, 
     PRIMARY KEY (id)
 );
-
-CREATE TABLE rol(
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NULL,
-    salary DECIMAL(5, 2),
-    department_id INT REFERENCES deparment (id),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NULL,
-    last_name VARCHAR(30) NULL,
-    role_id INT REFERENCES rol (id),
-    manager_id INT REFERENCES employee (id),
-    PRIMARY KEY (id)
-);
