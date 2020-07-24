4787a523475520eb115a68b2e0f454e702619d24
cherjios <slopez0286@gmail.com>
2020-07-23 19:41:14 -0700

    Adding data to burger table 1

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('hawaiian burgers',false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Backyard Burgers',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BINGO Burgers',false);
