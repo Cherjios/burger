7fd12f1b4c283a6455decacb9b3c551d942affde
cherjios <slopez0286@gmail.com>
2020-07-25 21:48:52 -0700

    Adding submit action to add burger

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -25,15 +25,14 @@ $(function() {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
-      var newCat = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
+      var newBurger = {
+        name: $("#ca").val().trim()
       };
   
       // Send the POST request.
-      $.ajax("/api/cats", {
+      $.ajax("/api/burgers", {
         type: "POST",
-        data: newCat
+        data: newBurger
       }).then(
         function() {
           console.log("created new burger");
