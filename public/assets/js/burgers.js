d116a2e40a8aa8fcf0d267a16de033dfb8f0202e
cherjios <slopez0286@gmail.com>
2020-07-26 12:15:00 -0700

    if and unless in handlesbars

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,7 +2,7 @@
 $(function() {
     $(".change-devoured").on("click", function(event) {
       var id = $(this).data("id");
-      var newDevourData = $(this).data("devoured");
+      var newDevourData = true;
   
       var newDevourObj = {
         devoured: newDevourData
