c5c4108ae2145b0705e145e54c9742683202f7a0
cherjios <slopez0286@gmail.com>
2020-07-25 19:18:56 -0700

    Adding router.get in controlles

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,14 @@ var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
 var burgerObjectFunctions = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burgerObjectFunctions.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
