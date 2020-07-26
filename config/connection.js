d3e4b8006c7d224b2b78be8136fd03ea32f35534
cherjios <slopez0286@gmail.com>
2020-07-25 18:10:46 -0700

    Adding connection to db

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,10 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
