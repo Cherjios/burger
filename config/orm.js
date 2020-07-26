02e6c5a6e18f756af49fbf81cc7b54f0a8f8b579
cherjios <slopez0286@gmail.com>
2020-07-25 18:19:44 -0700

    Adding select all function to orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,15 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
+
+// Object for all our SQL statement functions.
+var orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+      },
+}
