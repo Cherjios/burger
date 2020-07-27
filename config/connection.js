aab6d064fdfcbddb7b91374a2e646dce83c57290
cherjios <slopez0286@gmail.com>
2020-07-26 19:46:34 -0700

    adding connection to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-  });
+// for local testing
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "password",
+//     database: "burgers_db"
+//   });
 
+// for heroku
+var connection = mysql.createConnection(process.env.JAWSDB_URL)
   // Make connection.
 connection.connect(function(err) {
     if (err) {
